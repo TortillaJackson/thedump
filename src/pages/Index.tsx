21c403f894f4e3f72c9b269b3a42e96dd32f9d34
TortillaJackson <85754648+TortillaJackson@users.noreply.github.com>
2025-06-20 17:07:45 +0200

    Add files via upload

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,6 +3,8 @@ import { Hero } from "@/components/Hero";
 import { VenturePrinciples } from "@/components/VenturePrinciples";
 import { WhyVentureBuilding } from "@/components/WhyVentureBuilding";
 import { AITools } from "@/components/AITools";
+import { CustomerQuotes } from "@/components/CustomerQuotes";
+import { Traction } from "@/components/Traction";
 import { AboutAdvisor } from "@/components/AboutAdvisor";
 import { BookConsultation } from "@/components/BookConsultation";
 import { Navigation } from "@/components/Navigation";
@@ -12,9 +14,15 @@ const Index = () => {
     <div className="min-h-screen bg-white">
       <Navigation />
       <Hero />
-      <VenturePrinciples />
-      <WhyVentureBuilding />
+      <div id="prinzipien">
+        <VenturePrinciples />
+      </div>
+      <div id="vorteile">
+        <WhyVentureBuilding />
+      </div>
       <AITools />
+      <CustomerQuotes />
+      <Traction />
       <AboutAdvisor />
       <BookConsultation />
     </div>
